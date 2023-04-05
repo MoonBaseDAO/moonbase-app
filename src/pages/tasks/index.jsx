85ea3af57c1a06d1f57187134b2b704685bccf95
Roy Hodge Jr <118377684+modelomaker812@users.noreply.github.com>
2023-04-05 09:14:40 -0500

    Update index.jsx

--- src/pages/tasks/index.jsx
+++ src/pages/tasks/index.jsx
@@ -1,17 +1,17 @@
 import { Layout as DashboardLayout } from 'src/layouts/dashboard/layout';
 import Board from "@/components/tasks/board";
 import { ChatBubbleLeftEllipsisIcon } from '@heroicons/react/24/outline';
-import { useGetTasks, usePostTasks } from '@/hooks/useTaskApi';
+import { useGetTask, usePostTask } from '@/hooks/useTaskApi';
 import { useEffect } from 'react';
 // sneaky ninja..
 const TasksPage = () => {
-//  const { loading, data, getAxios } = useGetTasks();
-//  const { postAxios } = usePostTask();
-//  if (loading) return <h1>Loading</h1>;    
-//  const handleTask = async (task) => {
-//    await postAxios(task);
-//    await getAxios();
-//  };
+  const { loading, data, getAxios } = useGetTask();
+  const { postAxios } = usePostTask();
+  if (loading) return <h1>Loading</h1>;    
+  const handleTask = async (task) => {
+    await postAxios(task);
+    await getAxios();
+  };
   return (
     <>
       <div className="p-5">
