b6d234e0f6a6b4c8be86f3dd2d43f9bd558830e0
Roy Hodge Jr <118377684+modelomaker812@users.noreply.github.com>
2023-04-05 08:49:56 -0500

    Create getTask.js

---
+++ src/mock/getTask.js
@@ -0,0 +1,31 @@
+export default [
+  {
+    "name": "Backlog",
+    "items": [
+      {
+        "id": "1",
+        "priority": 0,
+        "title": "Storing Tasks In Tasks Tables",
+        "chat": 1,
+        "attachment": 2,
+        "assignees": [
+          {
+            "avt": "https://randomuser.me/api/portraits/men/75.jpg"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "In Progress",
+    "items": []
+  },
+  {
+    "name": "In Review",
+    "items": []
+  },
+  {
+    "name": "Completed",
+    "items": []
+  }
+]
